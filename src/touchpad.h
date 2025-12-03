3374477ced1c44a53ceb0d1dea20d81c156c884b
Zash60 <xath06@gmail.com>
2025-12-03 20:36:41 -0300

    Update touchpad.h

--- src/touchpad.h
+++ src/touchpad.h
@@ -8,68 +8,54 @@
 #include "gfx.h"
 #include "controller.h"
 
-// Forward declaration
-struct Emulator;
+struct Emulator; // Forward declaration
 
 typedef struct TouchAxis{
-    int x;
-    int y;
-    int r;
-    int inner_x;
-    int inner_y;
-    int origin_x;
-    int origin_y;
-    uint8_t state;
-    uint8_t active;
-    uint8_t h_latch;
-    uint8_t v_latch;
+    int x, y, r;
+    int inner_x, inner_y;
+    int origin_x, origin_y;
+    uint8_t state, active;
+    uint8_t h_latch, v_latch;
     SDL_FingerID finger;
-    SDL_FRect bg_dest;
-    SDL_FRect joy_dest;
+    SDL_FRect bg_dest, joy_dest;
     SDL_Texture* bg_tx;
     SDL_Texture* joy_tx;
 } TouchAxis;
 
-
-typedef struct  TouchButton{
+typedef struct TouchButton{
     SDL_Texture * texture;
     SDL_FRect dest;
     uint32_t id;
-    int x;
-    int y;
-    int r;
+    int x, y, r;
     uint8_t active;
     uint8_t auto_render;
     SDL_FingerID finger;
 } TouchButton;
 
-// Aumentado para incluir Menu (8 botões no total: A, B, X, Y, Select, Start, Menu)
-// Save/Load foram removidos daqui para o menu de pausa, mas o Menu Button é essencial
-#define TOUCH_BUTTON_COUNT 7
+// 6 Jogo + 1 Menu + 1 Save + 1 Load = 9 Botões
+#define TOUCH_BUTTON_COUNT 9
+
+// IDs Especiais
 #define BTN_ID_MENU 0x30000
+#define BTN_ID_SAVE 0x10000
+#define BTN_ID_LOAD 0x20000
 
 typedef struct TouchPad{
     uint16_t status;
-    TouchButton A;
-    TouchButton turboA;
-    TouchButton B;
-    TouchButton turboB;
-    TouchButton select;
-    TouchButton start;
-    TouchButton menu;
+    // Mapeamento direto para facilitar acesso
+    TouchButton A, turboA, B, turboB, select, start;
+    TouchButton menu, save, load;
     
     TouchButton* buttons[TOUCH_BUTTON_COUNT];
     TouchAxis axis;
     GraphicsContext* g_ctx;
-    struct Emulator* emulator;
+    struct Emulator* emulator; 
     TTF_Font * font;
     
-    // Flags para modo de edição
     uint8_t edit_mode; 
     TouchButton* selected_button;
 } TouchPad;
 
-// forward declaration
 struct JoyPad;
 
 void init_touch_pad(struct Emulator* emulator);
@@ -83,10 +69,8 @@ void touchpad_mapper(struct JoyPad* joyPad, SDL_Event* event);
 #define ANDROID_TOUCHPAD_MAPPER(JOYPAD, EVENT) touchpad_mapper(JOYPAD, EVENT)
 
 #else
-
 #define ANDROID_INIT_TOUCH_PAD(EMU)
 #define ANDROID_FREE_TOUCH_PAD()
 #define ANDROID_RENDER_TOUCH_CONTROLS(CTX)
 #define ANDROID_TOUCHPAD_MAPPER(JOYPAD, EVENT)
-
 #endif
