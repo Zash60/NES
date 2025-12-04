64a072de370c29df969a082d4395c92679df2d5b
Zash60 <xath06@gmail.com>
2025-12-04 11:45:49 -0300

    Update gfx.h

--- src/gfx.h
+++ src/gfx.h
@@ -3,6 +3,7 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <stdint.h>
+#include "ppu.h" // Necessário para acessar OAM
 
 // Variavel global para controle do filtro (0=Normal, 1=Scanlines)
 extern int video_filter_mode;
@@ -20,15 +21,19 @@ typedef struct GraphicsContext{
     int screen_height;
     float scale;
     int is_tv;
-
 } GraphicsContext;
 
 void free_graphics(GraphicsContext* ctx);
-
 void get_graphics_context(GraphicsContext* ctx);
 
-// Atualizado para receber o registrador de máscara (PPUMASK)
-void render_graphics(GraphicsContext* g_ctx, const uint32_t* buffer, float fps, uint8_t mask_reg);
+// Renderiza o buffer do jogo para a textura (NÃO chama present)
+void render_graphics_update(GraphicsContext* g_ctx, const uint32_t* buffer, uint8_t mask_reg);
 
-// Nova função para desenhar o jogo pausado sem glitches
+// Desenha as hitboxes por cima da textura atual
+void render_hitboxes(GraphicsContext* g_ctx, struct PPU* ppu);
+
+// Desenha FPS, Filtros, Controles e joga tudo na tela (Present)
+void render_ui_and_present(GraphicsContext* g_ctx, float fps);
+
+// Desenha apenas o frame atual (para pause)
 void render_frame_only(GraphicsContext* g_ctx);
