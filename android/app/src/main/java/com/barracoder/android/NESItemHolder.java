dd7e4ac0e86701bcc892c375a4fa5fd8e4b10def
Zash60 <xath06@gmail.com>
2025-12-03 21:35:06 -0300

    Update NESItemHolder.java

--- android/app/src/main/java/com/barracoder/android/NESItemHolder.java
+++ android/app/src/main/java/com/barracoder/android/NESItemHolder.java
@@ -8,9 +8,10 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NESItemHolder  extends RecyclerView.ViewHolder {
+public class NESItemHolder extends RecyclerView.ViewHolder {
     ImageView gameImage;
     TextView gameName;
+    TextView gameDetails; // Novo campo
     Button playButton;
     ImageButton magicButton;
     NESItemModel item;
@@ -19,6 +20,7 @@ public class NESItemHolder  extends RecyclerView.ViewHolder {
         super(itemView);
         gameImage = itemView.findViewById(R.id.gameImage);
         gameName = itemView.findViewById(R.id.gameName);
+        gameDetails = itemView.findViewById(R.id.gameDetails); // Bind do novo ID
         playButton = itemView.findViewById(R.id.playButton);
         magicButton = itemView.findViewById(R.id.magicButton);
     }
